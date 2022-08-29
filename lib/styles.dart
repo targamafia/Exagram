0a735d9a57a59281c2037a325114cc0fc73040f9
Fernando Melgar <fernandom.melgar@gmail.com>
2022-08-29 13:09:04 -0500

    Add Light Color Palette

--- lib/styles.dart
+++ lib/styles.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 ThemeData exagramThemeLight() {
   return ThemeData(
-      primaryColor: AppColor.blue200,
-      primarySwatch: Colors.red,
+      primaryColor: AppColorLight.primary,
+      errorColor: AppColorLight.error,
+      primarySwatch: AppColorLight.materialPrimary,
       textTheme: const TextTheme(
         headline1: TextStyle(fontSize: 40.0),
         headline2: TextStyle(fontSize: 33.0),
@@ -18,8 +19,40 @@ ThemeData exagramThemeDark() {
   return ThemeData();
 }
 
-class AppColor extends Color {
-  AppColor(super.value);
+class AppColorLight extends Color {
+  AppColorLight(super.value);
+
+  static const primary = Color(0xFF4059AD);
+  static const materialPrimary = MaterialColor(0xFF4059AD, {
+    50: Color.fromRGBO(64, 89, 173, .1),
+    100: Color.fromRGBO(64, 89, 173, .2),
+    200: Color.fromRGBO(64, 89, 173, .3),
+    300: Color.fromRGBO(64, 89, 173, .4),
+    400: Color.fromRGBO(64, 89, 173, .5),
+    500: Color.fromRGBO(64, 89, 173, .6),
+    600: Color.fromRGBO(64, 89, 173, .7),
+    700: Color.fromRGBO(64, 89, 173, .8),
+    800: Color.fromRGBO(64, 89, 173, .9),
+    900: Color.fromRGBO(64, 89, 173, 1),
+  });
+
+  static const onPrimary = Color(0xFFFFFFFF);
+  static const primaryContainer = Color(0xFFDCE1FF);
+  static const onPrimaryContainer = Color(0xFF00163F);
+
+  static const error = Color(0xFFBA1A1A);
+  static const onError = Color(0xFFFFFFFF);
+  static const errorContainer = Color(0xFFFFDAD6);
+  static const onErrorContainer = Color(0xFF410002);
+
+  static const background = Color(0xFFFEFBFF);
+  static const onBackground = Color(0xFF1B1B1F);
+  static const surface = Color(0xFFFEFBFF);
+  static const onSurface = Color(0xFF1B1B1F);
+
+  static const surfaceVariant = Color(0xFFE5E5E5);
+  static const onSurfaceVariant = Color(0xFF45464F);
+  static const outline = Color(0xFF767680);
 
   static const blue100 = Color(0xFF3EADCF);
   static const blue200 = Color(0xFFABE9CD);
